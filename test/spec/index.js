70c055e92d85128efbe7279b76e8697cf47d7201
bubkoo <bubkoo.wy@gmail.com>
2016-04-13 10:42:47 +0800

    test spec

--- test/spec/index.js
+++ test/spec/index.js
@@ -1,70 +1,53 @@
 'use strict';
 
-var char   = require('chai');
-var expect = char.expect;
+var expect = require('chai').expect;
 
 
 describe('is-array-like', function () {
 
-    var isArrayLike = require('../../index');
+  var isArrayLike = require('../../');
 
-    it('isArrayLike()', function () {
-        expect(isArrayLike()).to.be.false;
-    });
+  it('common test', function () {
+    expect(isArrayLike()).to.be.false;
 
-    it('isArrayLike(1)', function () {
-        expect(isArrayLike(1)).to.be.false;
-    });
+    expect(isArrayLike(1)).to.be.false;
 
-    it('isArrayLike("1")', function () {
-        expect(isArrayLike('1')).to.be.false;
-    });
+    expect(isArrayLike('1')).to.be.false;
 
-    it('isArrayLike(true)', function () {
-        expect(isArrayLike(true)).to.be.false;
-    });
+    expect(isArrayLike(true)).to.be.false;
 
-    it('isArrayLike(null)', function () {
-        expect(isArrayLike(null)).to.be.false;
-    });
+    expect(isArrayLike(null)).to.be.false;
 
-    it('isArrayLike({})', function () {
-        expect(isArrayLike({})).to.be.false;
-    });
+    expect(isArrayLike({})).to.be.false;
 
-    it('isArrayLike(function)', function () {
-        expect(isArrayLike(function () {})).to.be.false;
-    });
+    expect(isArrayLike(function () {})).to.be.false;
 
-    it('isArrayLike([])', function () {
-        expect(isArrayLike([])).to.be.true;
-    });
+    expect(isArrayLike([])).to.be.true;
 
-    it('isArrayLike([1, 2])', function () {
-        expect(isArrayLike([1, 2])).to.be.true;
-    });
+    expect(isArrayLike([1, 2])).to.be.true;
+  });
 
-    it('isArrayLike(arguments)', function () {
-        expect(isArrayLike(arguments)).to.be.true;
-    });
+  it('isArrayLike(arguments)', function () {
+    expect(isArrayLike(arguments)).to.be.true;
+  });
 
-    it('isArrayLike(nodeList)', function () {
+  it('isArrayLike(nodeList)', function () {
 
-        var nodeList = {};
+    var nodeList = {};
 
-        nodeList.nodeType = 1;
-        nodeList.length   = 1;
+    nodeList.nodeType = 1;
+    nodeList.length   = 1;
 
-        expect(isArrayLike(nodeList)).to.be.true;
-    });
+    expect(isArrayLike(nodeList)).to.be.true;
+  });
 
-    it('isArrayLike(arrayLike)', function () {
+  it('isArrayLike(arrayLike)', function () {
 
-        var arrayLike = {};
+    var arrayLike = {};
 
-        arrayLike['2']   = 'a';
-        arrayLike.length = 3;
+    arrayLike['2']   = 'a';
+    arrayLike.length = 3;
 
-        expect(isArrayLike(arrayLike)).to.be.true;
-    });
+    expect(isArrayLike(arrayLike)).to.be.true;
+  });
 });
